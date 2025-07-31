8c1053421dfbfac3152281e5bdb65518df5c34a2
Yukia <yuki.abiko1204@gmail.com>
2025-08-01 01:03:28 +0900

    管理者ページ追加

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -3,13 +3,11 @@
 import * as admin from "firebase-admin";
 
 if (!admin.apps.length) {
-  // 環境変数からサービスアカウント情報をJSONパースして取得
   const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || "{}");
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    // Realtime Databaseを使う場合は以下をコメント解除して設定
-    // databaseURL: "https://<your-project-id>.firebaseio.com",
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "koku-lesson-planner.firebasestorage.app",
   });
 }
 
