738f87d62ea8ecad133eb78a3e7644315c76a1f9
Yukia <yuki.abiko1204@gmail.com>
2025-08-01 00:20:44 +0900

    お問い合わせSDK

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -5,6 +5,7 @@ if (!admin.apps.length) {
   const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || "{}");
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,  // ここを追加
   });
 }
 
