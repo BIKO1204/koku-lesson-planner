604ab729e9502a8ee0a9fe1cef2cac5d5f0cbdfb
Yukia <yuki.abiko1204@gmail.com>
2025-08-02 12:04:13 +0900

    ストレージバケット消去

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+// next.config.js
+
 const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
@@ -5,4 +7,9 @@ const withPWA = require('next-pwa')({
 
 module.exports = withPWA({
   reactStrictMode: true,
+  env: {
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    GOOGLE_DRIVE_FOLDER_ID: process.env.GOOGLE_DRIVE_FOLDER_ID,
+    // 必要に応じて他の環境変数もここに追加
+  },
 });
